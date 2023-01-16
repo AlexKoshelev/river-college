9249cdc0e777b3bb861cf5430f4f5ea163bc834c
AlexKoshelev <2391382@mail.ru>
2023-01-16 08:54:38 +0300

    inter page

--- src/app/components/page/sveden/inter/Inter.jsx
+++ src/app/components/page/sveden/inter/Inter.jsx
@@ -1,5 +1,61 @@
 import React from "react";
 const Inter = () => {
-  return <div className="">Inter</div>;
+  return (
+    <div className="common__container">
+      <h1 className="common__container-title title">
+        Международное сотрудничество
+      </h1>
+      <h3 className="common__container-title title">
+        Сведения о заключенных и планируемых к заключению договорах с
+        иностранными и (или) международными организациями по вопросам
+        образования и науки:
+      </h3>
+      <table className="">
+        <tbody>
+          <tr>
+            <td>№ п/п</td>
+            <td>Государство</td>
+            <td>Наименование организации</td>
+            <td>
+              Реквизиты договора (наименование, дата, номер, срок действия)
+            </td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 className="common__container-title title">
+        Информация о международной аккредитации
+      </h3>
+      <table className="">
+        <tbody>
+          <tr>
+            <td>Код </td>
+            <td>
+              Наименование профессии, специальности, направления подготовки{" "}
+            </td>
+            <td>
+              Срок действия международной аккредитации (дата окончания действия
+              свидетельства о международной аккредитации)
+            </td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 className="common__container-title title">
+        Информация о заключенных и планируемых к заключению договорах с
+        иностранными и (или) международными организациями по вопросам
+        образования и науки - договора отсутствуют.
+      </h3>
+    </div>
+  );
 };
 export default Inter;
