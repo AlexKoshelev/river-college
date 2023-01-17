931fa5f30ae4b1a2b4bc496d44c8fca4c49799cf
AlexKoshelev <2391382@mail.ru>
2023-01-17 17:27:28 +0300

    employees, budget

--- src/app/components/page/sveden/eduStandarts/EduStandarts.jsx
+++ src/app/components/page/sveden/eduStandarts/EduStandarts.jsx
@@ -1,5 +1,94 @@
 import React from "react";
 const EduStandarts = () => {
-  return <div className="">Document</div>;
+  return (
+    <div className="common__container">
+      <h1 className="common__container-title title">
+        Образовательные стандарты и требования
+      </h1>
+      <h3 className="common__container-title title">
+        Федеральные государственные образовательные стандарты среднего
+        профессионального образования (ФГОС СПО) III + поколения
+      </h3>
+      <table className="">
+        <tbody>
+          <tr>
+            <td>Стандарт образовательной программы</td>
+            <td>Дисциплины и модули</td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <a href={"Ustav"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.03 Судовождение от 07.05.2014 № 441
+                  </li>
+                </a>
+                <a href={"UstavChange1415"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.03 Судовождение от 02.12.2020 № 691
+                  </li>
+                </a>
+              </ul>
+            </td>
+            <td> 26.02.03</td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <a href={"Ustav"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.05 Эксплуатация судовых энергетических установок
+                    от 07.05.2014 № 443
+                  </li>
+                </a>
+                <a href={"UstavChange1415"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.05 Эксплуатация судовых энергетических установок
+                    от 26.11.2020 № 674
+                  </li>
+                </a>
+              </ul>
+            </td>
+            <td> 26.02.05</td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <a href={"Ustav"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.06 Эксплуатация судового электрооборудования и
+                    средств автоматики от 07.05.2014 № 444
+                  </li>
+                </a>
+                <a href={"UstavChange1415"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 26.02.06 Эксплуатация судового электрооборудования и
+                    средств автоматики от 26.11.2020 № 675
+                  </li>
+                </a>
+              </ul>
+            </td>
+            <td> 26.02.06</td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <a href={"Ustav"} target={"_blank"} rel="noreferrer">
+                  <li className="doc__item active__link">
+                    ФГОС 23.02.01 Организация перевозок и управление на
+                    транспорте (по видам) от 22.04.2014 № 376
+                  </li>
+                </a>
+              </ul>
+            </td>
+            <td>23.02.01</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 className="common__container-title title">
+        Образовательные стандарты не разрабатываются и не реализуются.
+      </h3>
+    </div>
+  );
 };
 export default EduStandarts;
--- src/app/components/page/sveden/employees/Employees.jsx
+++ src/app/components/page/sveden/employees/Employees.jsx
@@ -1,5 +1,99 @@
 import React from "react";
 const Employees = () => {
-  return <div className="">employees</div>;
+  return (
+    <div className="common__container">
+      <h1 className="common__container-title title">
+        Руководство. Педагогический (научно-педагогический) состав
+      </h1>
+      <h3 className="common__container-title title">
+        Информация о руководителе/заместителях руководителя Уфимского филиала
+        ФГБОУ ВО "ВГУВТ"
+      </h3>
+      <div className="scroll-table">
+        <table className="">
+          <tbody>
+            <tr>
+              <td>ФИО</td>
+              <td>Должность</td>
+              <td>Контактные телефоны</td>
+              <td>Адреса электронной почты</td>
+            </tr>
+            <tr>
+              <td>Ахмадеева Фарида Шариповна </td>
+              <td>Директор </td>
+              <td>8 (347) 278-28-83 </td>
+              <td> ukru@ufanet.ru</td>
+            </tr>
+            <tr>
+              <td>Мусина Гульфия Ириковна </td>
+              <td>Заместитель директора </td>
+              <td>8 (347) 278-28-86 </td>
+              <td> gim_gim@mail.ru</td>
+            </tr>
+            <tr>
+              <td>Акбашева Альбина Галимзяновна </td>
+              <td>Заместитель директора </td>
+              <td>8 (347) 278-28-86 </td>
+              <td> anelya.ufa@mail.ru</td>
+            </tr>
+            <tr>
+              <td>Жоссан Елена Евгеньевна </td>
+              <td>Главный бухгалтер </td>
+              <td>8 (347) 278-03-25 </td>
+              <td> lenazhossan@mail.ru</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Начальник центра организационно-правовой работы </td>
+              <td>8 (347) 278-28-83, 278-28-86, вн.110 </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <h3 className="common__container-title title">
+        Информация о руководителях филиалов Уфимского филиала ФГБОУ ВО "ВГУВТ"
+        (при наличии)
+      </h3>
+      Уфимский филиал ФГБОУ ВО "ВГУВТ" филиалов не имеет.
+      <table className="">
+        <tbody>
+          <tr>
+            <td>Наименование филиала</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>ФИО</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Должность</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Контактные телефоны</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Адреса электронной почты</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 className="common__container-title title">
+        Информация о составе педагогических (научно-педагогических) работников
+        Уфимского филиала ФГБОУ ВО "ВГУВТ"
+      </h3>
+      <ul>
+        <a href={"Ustav"} target={"_blank"} rel="noreferrer">
+          <li className="doc__item active__link">
+            Информация о составе педагогических (научно-педагогических)
+            работников Уфимского филиала ФГБОУ ВО "ВГУВТ" ЭЦП
+          </li>
+        </a>
+      </ul>
+      <div className="scroll-table"></div>
+    </div>
+  );
 };
 export default Employees;
