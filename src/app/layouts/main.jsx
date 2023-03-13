3742120cc8033d38fab3a95bb91c03c6620276ef
AlexKoshelev <2391382@mail.ru>
2023-03-13 14:49:58 +0300

    live cards mobile

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -41,7 +41,7 @@ const Main = observer(() => {
           {params.element === "security" ? <SecurityPage /> : null}
         </div>
       </div>
-      {/* <LiveCardContainer />  */}
+      <LiveCardContainer />
     </>
   );
 });
